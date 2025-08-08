01fdd85ff700405dfc36cfbab7e925e2c46005aa
Alonso Naranjo González <alonso301090@gmail.com>
2025-08-08 00:27:18 -0600

    remove ignored content

--- Models/Entities/PropertyStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace InmobiliariaAPI.Models.Entities;
-
-public enum PropertyStatus
-{
-    Disponible = 1,
-    Vendida = 2,
-    Reservada = 3,
-    Inactiva = 4
-}
--- Models/Entities/PropertyType.cs
+++
@@ -1,9 +0,0 @@
-namespace InmobiliariaAPI.Models.Entities;
-
-public enum PropertyType
-{
-    Casa = 1,
-    Apartamento = 2,
-    Comercial = 3,
-    Terreno = 4
-}
